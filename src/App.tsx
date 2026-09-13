686ea5947aecdcc9be3ebb2149b8082cefc8888e
Hanjala13 <mdhanjala0224@gmail.com>
2026-09-14 00:59:45 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from "./assets/components/Nav"
+
+function App() {
+
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Nav.tsx
@@ -0,0 +1,11 @@
+
+
+const Nav = () => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    );
+};
+
+export default Nav;
